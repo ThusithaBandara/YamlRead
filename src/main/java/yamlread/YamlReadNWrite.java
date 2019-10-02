06897885d3e5e868de7d197aad9ac599408bb501
ThusithaBandara <thusitharmtgk95@gmail.com>
2019-10-02 10:04:30 +0530

    Yaml file reader

---
+++ src/main/java/yamlread/YamlReadNWrite.java
@@ -0,0 +1,54 @@
+package yamlread;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.function.BiFunction;
+
+import org.yaml.snakeyaml.Yaml;
+
+
+
+
+public class YamlReadNWrite {
+
+
+	private static Yaml yaml;
+
+	public static void main(String[] args) {
+		yaml = new Yaml();
+	
+		InputStream inputStream = yaml.getClass().getClassLoader().getResourceAsStream("master.yaml");
+		
+		Map<String, Object> obj_1 = (Map<String, Object>) yaml.load(inputStream);
+		 
+	
+		InputStream input2 =yaml.getClass().getClassLoader().getResourceAsStream("Second.yaml");
+		Map<String, Object> obj_2 = (Map<String, Object>) yaml.load(input2);
+		  
+		for (Map.Entry n : obj_2.entrySet()) { 
+			obj_1.replace((String) n.getKey(),n.getValue());
+			obj_1.putIfAbsent((String) n.getKey(), n.getValue());
+			
+		  
+		} 
+		
+		System.out.println(obj_1);
+		 
+		for(Map.Entry n: obj_1.entrySet()) {
+			System.out.println(n.getKey()+"	"+n.getValue());
+		}
+		
+			
+		
+
+	}
+
+	
+}
